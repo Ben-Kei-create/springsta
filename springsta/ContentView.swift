1e0a06b0ee58c196b24afd15119e231c9ae28b08
Ben-Kei-create <112257840+Ben-Kei-create@users.noreply.github.com>
2026-05-30 00:15:34 +0900

    Initial Commit

---
+++ springsta/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  springsta
+//
+//  Created by 茂木史明 on 2026/05/30.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ springsta/springstaApp.swift
@@ -0,0 +1,17 @@
+//
+//  springstaApp.swift
+//  springsta
+//
+//  Created by 茂木史明 on 2026/05/30.
+//
+
+import SwiftUI
+
+@main
+struct springstaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
